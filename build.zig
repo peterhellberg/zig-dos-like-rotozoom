a6c75c83c699540254413c2b27986f0028527ea9
Peter Hellberg <peter@c7.se>
2025-08-27 14:00:14 +0200

    build: Use b.createModule

--- build.zig
+++ build.zig
@@ -6,9 +6,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "zig-dos-like-rotozoom",
-        .root_source_file = b.path("src/rotozoom.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/rotozoom.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const dos = b.dependency("dos", .{
